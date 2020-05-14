a0060dabcf2a37ac43e027a06883f516dfe46584
akbersalim <akbersalim123@gmail.com>
2020-05-15 01:38:29 +0530

    singlechildscrollviewadded

--- lib/views/add.dart
+++ lib/views/add.dart
@@ -14,10 +14,16 @@ class Calculate extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
               SingleChildScrollView(
-                child: Container(
-                    height: MediaQuery.of(context).size.height-630,
-                    child: SingleChildScrollView(child: MyBody())),
-              )
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                        height: MediaQuery.of(context).size.height - 627,
+                        color: Colors.white,
+                        margin: EdgeInsets.all(5.0),
+                        child: SingleChildScrollView(child: MyBody())),
+                  ],
+                ),
+              ),
             ],
           )),
     );
@@ -35,6 +41,20 @@ class _MyBodyState extends State<MyBody> {
   TextEditingController n2 = TextEditingController();
   var col1 = 0xff373B44, col2 = 0xff4286f4;
   var bck = 0xfffffffff;
+
+  Container res() {
+    return Container(
+      height: 50.0,
+      width: double.infinity,
+      child: Center(
+        child: Text(
+          _x.toString(),
+          style: TextStyle(fontSize: 23.0, color: Colors.black),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
 
   _Compute() {
     var num1 = double.parse(n1.text);
@@ -60,182 +80,189 @@ class _MyBodyState extends State<MyBody> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Color(bck),
-      margin: EdgeInsets.all(20.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          TextField(
-            keyboardType: TextInputType.number,
-            controller: n1,
-            textAlign: TextAlign.center,
-            decoration: InputDecoration(
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10.0),
-                borderSide: BorderSide(color: Colors.greenAccent, width: 2.0),
+    return Column(
+      children: <Widget>[
+        Container(
+          color: Color(bck),
+          margin: EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("Scroll down"),
+              SizedBox(
+                height: 10.0,
               ),
-              hintText: "Number 1",
-              labelText: "Enter a number",
-              border:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(10.0)),
-            ),
-          ),
-          SizedBox(
-            height: 10.0,
-          ),
-          Container(
-              height: 50.0,
-              width: double.infinity,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  GestureDetector(
-                    onTap: () {
-                      choice = 1;
-                      setState(() {
-                        _Compute();
-                        col1 = 0xff8A2387;
-                        col2 = 0xffE94057;
-                        col2 = 0xffF27121;
-                      });
-                    },
-                    child: Container(
-                      child: Center(
-                          child: Text("+",
-                              style: TextStyle(
-                                  color: Colors.white, fontSize: 30.0))),
-                      height: double.infinity,
-                      width: MediaQuery.of(context).size.width / 5,
-                      decoration: BoxDecoration(
-                          color: Colors.black,
-                          borderRadius: BorderRadius.circular(10.0)),
-                    ),
-                  ),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      choice = 2;
-                      setState(() {
-                        _Compute();
-                        col1 = 0xff1f4037;
-                        col2 = 0xff99f2c8;
-                      });
-                    },
-                    child: Container(
-                      child: Center(
-                          child: Text("-",
-                              style: TextStyle(
-                                  color: Colors.white, fontSize: 30.0))),
-                      height: double.infinity,
-                      width: MediaQuery.of(context).size.width / 5,
-                      decoration: BoxDecoration(
-                          color: Colors.black,
-                          borderRadius: BorderRadius.circular(10.0)),
-                    ),
-                  ),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      choice = 3;
-                      setState(() {
-                        _Compute();
-                        col1 = 0xffe1eec3;
-                        col2 = 0xfff05053;
-                      });
-                    },
-                    child: Container(
-                      child: Center(
-                          child: Text("x",
-                              style: TextStyle(
-                                  color: Colors.white, fontSize: 30.0))),
-                      height: double.infinity,
-                      width: MediaQuery.of(context).size.width / 5,
-                      decoration: BoxDecoration(
-                          color: Colors.black,
-                          borderRadius: BorderRadius.circular(10.0)),
-                    ),
-                  ),
-                  SizedBox(width: 10.0),
-                  GestureDetector(
-                    onTap: () {
-                      choice = 4;
-                      setState(() {
-                        _Compute();
-                        col1 = 0xffFF0099;
-                        col2 = 0xff493240;
-                      });
-                    },
-                    child: Container(
-                      child: Center(
-                          child: Text("÷",
-                              style: TextStyle(
-                                  color: Colors.white, fontSize: 30.0))),
-                      height: double.infinity,
-                      width: MediaQuery.of(context).size.width / 5,
-                      decoration: BoxDecoration(
-                          color: Colors.black,
-                          borderRadius: BorderRadius.circular(10.0)),
-                    ),
-                  ),
-                ],
-              )),
-          SizedBox(
-            height: 10.0,
-          ),
-          TextField(
-            keyboardType: TextInputType.number,
-            controller: n2,
-            textAlign: TextAlign.center,
-            decoration: InputDecoration(
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  borderSide: BorderSide(color: Colors.greenAccent, width: 2.0),
-                ),
-                labelText: "Enter a number",
-                hintText: "Number 2",
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0))),
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          InkWell(
-            child: Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.0),
-                  gradient: LinearGradient(
-                    colors: [Color(col1), Color(col2)],
-                  )),
-              height: 50.0,
-              width: double.infinity,
-              child: Center(
-                child: Text(
-                  "=",
-                  style: TextStyle(fontSize: 23.0, color: Colors.white),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ),
-          ),
-          Container(
-            height: 50.0,
-            width: double.infinity,
-            child: Center(
-              child: Text(
-                _x.toString(),
-                style: TextStyle(fontSize: 23.0, color: Colors.black),
+              TextField(
+                keyboardType: TextInputType.number,
+                controller: n1,
                 textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide:
+                        BorderSide(color: Colors.greenAccent, width: 2.0),
+                  ),
+                  hintText: "Number 1",
+                  labelText: "Enter a number",
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0)),
+                ),
               ),
-            ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                  height: 50.0,
+                  width: double.infinity,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      GestureDetector(
+                        onTap: () {
+                          choice = 1;
+                          setState(() {
+                            _Compute();
+                            col1 = 0xff8A2387;
+                            col2 = 0xffE94057;
+                            col2 = 0xffF27121;
+                          });
+                        },
+                        child: Container(
+                          child: Center(
+                              child: Text("+",
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 30.0))),
+                          height: double.infinity,
+                          width: MediaQuery.of(context).size.width / 5,
+                          decoration: BoxDecoration(
+                              color: Colors.black,
+                              borderRadius: BorderRadius.circular(10.0)),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          choice = 2;
+                          setState(() {
+                            _Compute();
+                            col1 = 0xff1f4037;
+                            col2 = 0xff99f2c8;
+                          });
+                        },
+                        child: Container(
+                          child: Center(
+                              child: Text("-",
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 30.0))),
+                          height: double.infinity,
+                          width: MediaQuery.of(context).size.width / 5,
+                          decoration: BoxDecoration(
+                              color: Colors.black,
+                              borderRadius: BorderRadius.circular(10.0)),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          choice = 3;
+                          setState(() {
+                            _Compute();
+                            col1 = 0xffe1eec3;
+                            col2 = 0xfff05053;
+                          });
+                        },
+                        child: Container(
+                          child: Center(
+                              child: Text("x",
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 30.0))),
+                          height: double.infinity,
+                          width: MediaQuery.of(context).size.width / 5,
+                          decoration: BoxDecoration(
+                              color: Colors.black,
+                              borderRadius: BorderRadius.circular(10.0)),
+                        ),
+                      ),
+                      SizedBox(width: 10.0),
+                      GestureDetector(
+                        onTap: () {
+                          choice = 4;
+                          setState(() {
+                            _Compute();
+                            col1 = 0xffFF0099;
+                            col2 = 0xff493240;
+                          });
+                        },
+                        child: Container(
+                          child: Center(
+                              child: Text("÷",
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 30.0))),
+                          height: double.infinity,
+                          width: MediaQuery.of(context).size.width / 5,
+                          decoration: BoxDecoration(
+                              color: Colors.black,
+                              borderRadius: BorderRadius.circular(10.0)),
+                        ),
+                      ),
+                    ],
+                  )),
+              SizedBox(
+                height: 10.0,
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                controller: n2,
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide:
+                          BorderSide(color: Colors.greenAccent, width: 2.0),
+                    ),
+                    labelText: "Enter a number",
+                    hintText: "Number 2",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0))),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              InkWell(
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      gradient: LinearGradient(
+                        colors: [Color(col1), Color(col2)],
+                      )),
+                  height: 50.0,
+                  width: double.infinity,
+                  child: Center(
+                    child: Text(
+                      "=",
+                      style: TextStyle(fontSize: 23.0, color: Colors.white),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
+        ),
+        Text(
+          _x.toString(),
+          style: TextStyle(fontSize: 30.0),
+        )
+      ],
     );
   }
 }
+
+/*
+
+ */
